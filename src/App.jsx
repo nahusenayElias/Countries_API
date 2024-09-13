3d0beffc75eb99626484ff7c0b8082d6f424ed53
Elias Hagos <eliasars@yahoo.com>
2024-09-13 13:44:25 +0300

    Updates on Countries React Redux project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./component/Home";
+import ErrorPage from "./component/ErrorPage";
+import Countries from "./component/Countries";
+import CountrySingle from "./component/CountrySingle";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-       
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route>
+            {/* other routes will be here */}
+            <Route path="/" element={<Home />} />
+            <Route path="/countries" element={<Countries />} />
+            <Route path="countries/:single" element={<CountrySingle />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
