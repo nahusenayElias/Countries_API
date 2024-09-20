79739ee63cdb48767101e6b62a992f2bc78eba00
Elias Hagos <eliasars@yahoo.com>
2024-09-20 12:14:35 +0300

    Added Register component, log in and data will be stored in Cloud Firestore DB

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./component/Home";
 import ErrorPage from "./component/ErrorPage";
 import Countries from "./component/Countries";
 import CountrySingle from "./component/CountrySingle";
+import Register from "./component/Register";
 
 const App = () => {
   return (
@@ -12,6 +13,8 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route>
             {/* other routes will be here */}
+            <Route path="/register" element={<Register />} />
+
             <Route path="/" element={<Home />} />
             <Route path="/countries" element={<Countries />} />
             <Route path="countries/:single" element={<CountrySingle />} />
