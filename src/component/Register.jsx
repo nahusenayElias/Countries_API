dc71efbd8beb2b13cfd61e38434a652da87d89e4
Elias Hagos <eliasars@yahoo.com>
2024-10-07 21:52:08 +0300

    Updates and UI improvements

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -22,27 +22,41 @@ const Register = () => {
   //TODO: Add a check to see if user is logged in and navigate to countries if logged in.
 
   return (
-    <div>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Full name"
-      />
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="email"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="password"
-      />
-      <Button onClick={handleRegister}>Register</Button>
-    </div>
+    <section className="h-100 gradient-form">
+      <div className="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-xl-10">
+            <div className="card rounded-3 text-black">
+              <div>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Full name"
+                />
+                <br />
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="email"
+                />
+                <br />
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="password"
+                />
+                <br />
+                <Button onClick={handleRegister}>Register</Button>
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 export default Register;
