7b90a91686ffafa710c59594231e939bb114aef9
Elias Hagos <eliasars@yahoo.com>
2024-10-07 21:46:59 +0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,32 @@ import ErrorPage from "./component/ErrorPage";
 import Countries from "./component/Countries";
 import CountrySingle from "./component/CountrySingle";
 import Register from "./component/Register";
+import Login from "./component/Login";
+import { useAuthState } from "react-firebase-hooks/auth";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import { auth } from "./auth/firebase";
+import Favourites from "./component/Favourites";
 
-const App = () => {
+
+function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route>
             {/* other routes will be here */}
-            <Route path="/register" element={<Register />} />
-
             <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+
+            <Route element={<ProtectedRoute user={user} />}>
             <Route path="/countries" element={<Countries />} />
+            <Route path="/favourites" element={<Favourites />} />
             <Route path="countries/:single" element={<CountrySingle />} />
-            <Route path="*" element={<ErrorPage />} />
           </Route>
+
+            <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
