d9ae4a29b0e16c627eed37fb47ce4df539579132
Elias Hagos <eliasars@yahoo.com>
2024-10-07 21:56:01 +0300

    Updates on favouritesSlices

--- src/store/favouritesSlice.js
+++ src/store/favouritesSlice.js
@@ -1,7 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addFavouriteToFirebase, auth, db } from "../auth/firebase";
+import { collection, getDocs, query } from "firebase/firestore";
 
 const initialState = {
   favourites: [],
+  isLoading: true,
 
 };
 export const favouritesSlice = createSlice({
@@ -9,18 +12,39 @@ export const favouritesSlice = createSlice({
   initialState,
   reducers: {
      addFavourite(state, action) {
-        state.favourites = [...state.favourites,action.payload]
+        state.favourites = [...state.favourites,action.payload];
+        const user = auth.currentUser;
+        if (user) addFavouriteToFirebase(user.uid, action.payload)
      },
      clearFavourites(state) {
         state.favourites = []
      },
      removeFavourite(state, action) {
-        state.favourites = state.favourites.filter(
-         (favourite) => favourite !== action.payload
-     )
-     }
-  },
+        state.favourites = state.favourites.filter
+         (favourite => favourite !== action.payload)
+
+     },
+     getFavouritesFromSource(state, action) {
+      state.favourites = action.payload;
+},
+    isLoading(state, action) {
+      state.isLoading = action.payload;
+    }
+
+}
 });
-export const { addFavourite, clearFavourites, removeFavourite } = favouritesSlice.actions;
+
+export const getFavouritesFromSource = () =>  async (dispatch) => {
+   const user = auth.currentUser;
+   if (user){
+      const q = query(collection(db, `users/${user.uid}/favourites`))
+      const querySnapShot = await getDocs(q);
+      const favourites = querySnapShot.docs.map((doc) => doc.data().name);
+      dispatch(getFavourites(favourites));
+      dispatch(isLoading(false))
+   }
+
+}
+export const { addFavourite, clearFavourites, getFavourites, isLoading, removeFavourite } = favouritesSlice.actions;
 
 export default favouritesSlice.reducer;
