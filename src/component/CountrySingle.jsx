7f5006bc1b3351e4fffce52a634169981c35b67f
Elias Hagos <eliasars@yahoo.com>
2024-10-21 21:14:02 +0300

    Implement RF100

--- src/component/CountrySingle.jsx
+++ src/component/CountrySingle.jsx
@@ -11,8 +11,8 @@ const CountrySingle = (props) => {
 
   const navigate = useNavigate();
 
- // console.log("location", location);
- // console.log("Weather: ", weather);
+  // console.log("location", location);
+  // console.log("Weather: ", weather);
 
   useEffect(() => {
     axios
@@ -21,9 +21,9 @@ const CountrySingle = (props) => {
           country.capital
         }&units=metric&appid=${import.meta.env.VITE_WEATHER_API_KEY}`
       )
-      .catch((error) =>{
-      console.log(error);
-  })
+      .catch((error) => {
+        console.log(error);
+      })
       .then((response) => {
         setWeather(response.data);
         setWeatherLoading(false);
@@ -35,42 +35,50 @@ const CountrySingle = (props) => {
     return (
       <Col className="text-center-m-5">
         <Spinner
-        animation="border"
-        role="status"
-        className="center"
-        variant="info">
+          animation="border"
+          role="status"
+          className="center"
+          variant="info"
+        >
           <span className="visually-hidden">Loading...</span>
-
         </Spinner>
       </Col>
-    )
-
-    }
-return(
-
+    );
+  }
+  return (
     <Container fluid>
-        <Row>
-            <Col className="mt-5 d-flex justify-content-center">
-                <Image src={country.flags.svg} />
-            </Col>
-            <Col>
-                <h2>{country.name.common}</h2>
-                <h3>{country.capital}</h3>
-                <div>
-                  <p>
-                    Right now it is <strong>{parseInt(weather.main.temp )} </strong>
-                    degrees in {country.capital} and {weather.weather[0].description}
-                  </p>
-                  <Image src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
-                </div>
-                <Button variant="light" onClick={() => navigate("/countries")}>
+      <Row>
+        <Col className="mt-5 d-flex justify-content-center">
+          <Image src={country.flags.svg}
+               style={{
+                maxWidth: '450px',
+                maxHeight: '300px',
+                objectFit: 'contain',
+                border: '1px solid #ddd',
+                padding: '10px',
+                backgroundColor: '#f8f9fa',
+              }}
+          />
+        </Col>
+        <Col>
+          <h2>{country.name.common}</h2>
+          <h3>{country.capital}</h3>
+          <div>
+            <p>
+              Right now it is <strong>{parseInt(weather.main.temp)} </strong>
+              degrees in {country.capital} and {weather.weather[0].description}
+            </p>
+            <Image
+              src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            />
+          </div>
+          <Button variant="success" onClick={() => navigate("/countries")}>
             Back to Countries
           </Button>
-            </Col>
-        </Row>
-
+        </Col>
+      </Row>
     </Container>
-        )
+  );
 };
 
 export default CountrySingle;
