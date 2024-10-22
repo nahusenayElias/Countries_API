992217d14dfc23516d154fbcc5ed151231209db3
Elias Hagos <eliasars@yahoo.com>
2024-10-22 12:11:20 +0300

    Home page content update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div>countries app</div>;
+  return (<div className="text-center">
+    <h2 >Welcome to the Home page</h2>
+    <p>This is the Home page for countries App</p>
+
+  </div>);
 };
 
 export default Home;
