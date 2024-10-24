89b65d6e4bb0c3026466067be4294f79b2e031e6
Elias Hagos <eliasars@yahoo.com>
2024-10-24 20:52:57 +0300

    Home page bug fix

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,9 +1,10 @@
 const Home = () => {
-  return (<div className="text-center">
-    <h2 >Welcome to the Home page</h2>
-    <p>This is the Home page for countries App</p>
-
-  </div>);
+  return (
+    <div className="text-center">
+      <h2>Welcome to the Home page</h2>
+      <p>This is the Home page for countries App</p>
+    </div>
+  );
 };
 
 export default Home;
